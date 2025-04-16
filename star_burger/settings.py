01d1cb0c61f15fbdecef74b5f1dde6c95e24c0ba
LaryBary174 <lotuc74@gmail.com>
2025-04-16 22:46:45 +0500

    настройки подключения к дб в одном URL

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -85,20 +85,9 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     )
-# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=env('DB_URL'))
 }
 AUTH_PASSWORD_VALIDATORS = [
     {
